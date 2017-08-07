d4e2b65cdaa5b6c187294cd20c1a554fe75844b4
Ole Begemann <ole@oleb.net>
2017-08-07 21:38:16 +0200

    reduce with inout is now implemented (SE-0171)

--- Whats-new-in-Swift-4.playground/Pages/reduce with inout.xcplaygroundpage/Contents.swift
+++ Whats-new-in-Swift-4.playground/Pages/reduce with inout.xcplaygroundpage/Contents.swift
@@ -6,21 +6,18 @@
  [SE-0171][SE-0171] adds a variant of `reduce` in which the partial result is passed `inout` to the combine function. This can be a significant performance boost for algorithms that use `reduce` to incrementally build a sequence by eliminating copies of the intermediate results.
 
  [SE-0171]: https://github.com/apple/swift-evolution/blob/master/proposals/0171-reduce-with-inout.md "Swift Evolution Proposal SE-0171: Reduce with `inout`"
- 
- SE-0171 has recently been implemented, but it hasn’t landed in Xcode 9 yet.
  */
-// Not implemented yet
-//extension Sequence where Element: Equatable {
-//    func removingConsecutiveDuplicates() -> [Element] {
-//        return reduce(into: []) { (result: inout [Element], element) in
-//            if result.last != element {
-//                result.append(element)
-//            }
-//        }
-//    }
-//}
-//
-//[1,1,1,2,3,3,4].uniq()
+extension Sequence where Element: Equatable {
+    func removingConsecutiveDuplicates() -> [Element] {
+        return reduce(into: []) { (result: inout [Element], element) in
+            if result.last != element {
+                result.append(element)
+            }
+        }
+    }
+}
+
+[1,1,1,2,3,3,4].removingConsecutiveDuplicates()
 
 /*:
  [Table of contents](Table%20of%20contents) • [Previous page](@previous) • [Next page](@next)
